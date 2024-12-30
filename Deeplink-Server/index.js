1bca0211b4df0e085fc82d2b2f6ca8c02b49ebaf
dharmiksonani <dharmiksonani84618461@gmail.com>
2024-12-30 10:41:45 +0530

    Deeplink-Server-Setup

---
+++ Deeplink-Server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve .well-known/assetlinks.json
+app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
+
+// Default route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Node.js server for App Links!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
